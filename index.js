82197ce907e4622ab52d1d163d784470df553083
kamran-8674 <connect4kamran@gmail.com>
2025-11-12 14:54:25 +0600

    added delete funtionality

--- index.js
+++ index.js
@@ -77,7 +77,14 @@ async function run() {
   } )
    
   //  DELETE
- 
+   app.delete('/reviews/:id', async (req, res)=>{
+    const id = req.params.id
+    const query = {
+      _id: new ObjectId(id)
+    }
+    const result = await reviewsCollection.deleteOne(query)
+    res.send(result)
+   })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
